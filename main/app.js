c1e5792834cf433ad043aeb6de1a339148336a3d
Luisa Balaban <67561596+LuisaBalaban@users.noreply.github.com>
2020-11-03 23:07:26 +0200

    Update app.js

--- main/app.js
+++ main/app.js
@@ -1,6 +1,28 @@
 function removeOrderItem(orderInfo, position){
+       if(orderInfo.items.length>=0)
+        {
+            if(orderInfo.items.length>=position)
+               { 
+                  if(orderInfo.items.hasOwnProperty('quantity')==true && orderInfo.items.hasOwnProperty('price')==true)
+                   { 
+                       //last 2 tests work if we comment everything except this {}
+                    const obj=orderInfo.items[position];
+                    orderInfo.total-=obj.price*obj.quantity;
+                    orderInfo.items.splice(position, 1);
+                    return orderInfo;
+                   }
+            else throw('Malformed item');
+                  
+           } else throw('Invalid position');     
+            
+            
+        } else  throw('Items should be an array');
+        
+         
     
 }
+
+
 
 const app = {
     removeOrderItem
